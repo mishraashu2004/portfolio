062c022f62e1172c96ad4a2598f4d1b7556566d2
mishraashu2004 <142982037+mishraashu2004@users.noreply.github.com>
2024-09-27 20:52:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+
+document.getElementById('mobile-menu-toggle').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.nav-menu').classList.toggle('active');
+});
