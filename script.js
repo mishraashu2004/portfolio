8235c47962f1e9756f2bece2040fe0a9650c819e
mishraashu2004 <ashutoshmishra79753@gmail.com>
2025-08-22 00:34:43 +0530

    portfolio update

--- script.js
+++
@@ -1,7 +0,0 @@
-
-
-
-document.getElementById('mobile-menu-toggle').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.querySelector('.nav-menu').classList.toggle('active');
-});
